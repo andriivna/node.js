b9cb53e551190eb20f139ae536beea7240ec4052
andriivna <farinaanna686@gmail.com>
2022-11-11 10:42:13 +0200

    node js

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises');
+const path = require('path')
 
-fs.readdir('./students/boys', (err, files)=>{
-    console.log(files)
-    for (const fileName of files) {
-        console.log(files)
-        fs.stat(`./students/boys/${fileName}`,(err, stats)=>{
 
-            if (stats.isFile()){
-                fs.readFile(`./students/boys/${fileName}`, (err, data)=>{
-                    if ()
-                })
-            }
-        })
+const foo = async () => {
+    const folderPath = path.join(__dirname, 'girls')
+    const files = await fs.readdir(folderPath)
 
+    for (const file of files) {
+        const filePath = path.join(folderPath, file)
+        const data = await fs.readFile(filePath);
+        const user = JSON.parse(data);
+
+        if (user.gender === 'male'){
+            await fs.rename(filePath, path.join(__dirname, 'boys', file))
+        }
     }
-})
+
+};
+
+const foo1 = async () => {
+    const folderPath = path.join(__dirname, 'boys')
+    const files = await fs.readdir(folderPath)
+
+    for (const file of files) {
+        const filePath = path.join(folderPath, file)
+        const data = await fs.readFile(filePath);
+        const user = JSON.parse(data);
+
+        if (user.gender === 'female'){
+        await fs.rename(filePath, path.join(__dirname, 'girls', file))
+        }
+    }
+
+};
+foo()
+foo1()
+
+// fs.readdir('./boys', (err, files)=>{
+//     console.log(files)
+//     for (const fileName of files) {
+//         console.log(files)
+//         fs.stat(`./boys/${fileName}`,(err, stats)=>{
+//
+//             if (stats.isFile()){
+//                 fs.readFile(`./boys/${fileName}`, (err, data)=>{
+//                     if ()
+//                 })
+//             }
+//         })
+//
+//     }
+// })
