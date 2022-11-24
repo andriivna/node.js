0ea555dddd1885c2d9add11909245f9a23334426
andriivna <farinaanna686@gmail.com>
2022-11-24 12:23:09 +0200

    lesson 2 needs finishing

--- app.js
+++ app.js
@@ -1,35 +1,96 @@
 const express = require('express');
-
-const userDb = require('./dataBase/users')
+const { fileServices } = require('./services');
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('USERS ENDPOINT!')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    // res.json({
-    //     user:"Anna"
-    // })
+app.get('/users', async (req, res) => {
+    const users = await fileServices.reader();
+    res.json(users);
+});
 
-    //res.end('ITS OK')// закінчує запит чимось(тільки стрічкою, не об'єктом)
-    //res.status(402).json('ITS OK')
-    //res.sendFile('./') // Посилає файли(рідко використовується, або займається певна людина,яка працює з файлими)
+app.post('/users', async (req, res) => {
+    const userInfo = req.body;
 
-    res.json(userDb)
+    if (userInfo.name.length < 3 || typeof userInfo.name !== 'string') {
+        return res.status(400).json('Wrong name');
+    }
 
-}); //по дефолту браузер вміє тільки виконувати метод get
+    if (userInfo.age < 0 || Number.isNaN(+userInfo.age)) {
+        return res.status(400).json('Wrong age');
+    }
 
-app.get('/users/:userId',(req, res)=>{
+    const users = await fileServices.reader();
 
-    const{userId}=req.params
+    const newUser = {
+        name: userInfo.name,
+        age: userInfo.age,
+        id: users[users.length -1].id + 1
+    };
+    users.push(newUser);
 
-    res.json(userDb[userId]);
-})
+    await fileServices.writer(users);
 
-// app.get('/',(req, res)=>{
-//     res.json("WELCOME")
-// })
+    res.status(201).json(newUser);
+});
 
-app.listen(5000, ()=>{  //слухає порт, сказувати порт який потрібно прослухати
-    console.log('Server listen 5000')
-})
+app.get('/users/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    const users = await fileServices.reader();
+
+    const user = users.find((u) => u.id === +userId);
+
+    if (!user) {
+        return res.status(404).json(`User with id ${userId} not found`);
+    }
+
+    res.json(user);
+});
+
+app.put('/users/:userId', async (req, res) => {
+    const newUserInfo = req.body;
+    const { userId } = req.params;
+
+    const users = await fileServices.reader();
+
+    const index = users.findIndex((u) => u.id === +userId);
+
+    if (index === -1) {
+        return res.status(404).json(`User with id ${userId} not found`);
+    }
+
+    users[index] = { ...users[index], ...newUserInfo };
+
+    await fileServices.writer(users);
+
+    res.status(201).json(users[index]);
+});
+
+app.delete('/users/:userId', async (req, res) => {
+    const { userId } = req.params;
+
+    const users = await fileServices.reader();
+
+    const index = users.findIndex((u) => u.id === +userId);
+
+    if (index === -1) {
+        return res.status(404).json(`User with id ${userId} not found`);
+    }
+
+    users.splice(index, 1);
+
+    await fileServices.writer(users);
+
+    res.sendStatus(204);
+});
+
+app.get('/', (req, res) => {
+    res.json('WELOCME')
+});
+
+app.listen(5000, () => {
+    console.log('Server listen 5000');
+});
